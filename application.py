c2d93580cf96434373646a3be44c8046f6bcf59f
mamunkhancse <mdmamunkhan3295@gmail.com>
2024-01-09 00:30:24 +0600

    added

--- application.py
+++ application.py
@@ -1,11 +1,17 @@
-from function import *
+from functions import *
 from keras.utils import to_categorical
+
 from keras.models import model_from_json
+
 from keras.layers import LSTM, Dense
+
 from keras.callbacks import TensorBoard
+
 json_file = open("model.json", "r")
 model_json = json_file.read()
 json_file.close()
+
+
 model = model_from_json(model_json)
 model.load_weights("model.h5")
 
@@ -22,39 +28,42 @@ def prob_viz(res, actions, input_frame, colors,threshold):
     return output_frame
 
 
-# 1. New detection variables
 sequence = []
 sentence = []
 accuracy=[]
 predictions = []
 threshold = 0.8 
 
+
+
 cap = cv2.VideoCapture(0)
-# cap = cv2.VideoCapture("https://192.168.43.41:8080/video")
-# Set mediapipe model 
+
+
+
 with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
     while cap.isOpened():
 
-        # Read feed
+      
         ret, frame = cap.read()
 
-        # Make detections
-        cropframe=frame[40:400,0:300]
-        # print(frame.shape)
-        frame=cv2.rectangle(frame,(0,40),(300,400),255,2)
-        # frame=cv2.putText(frame,"Active Region",(75,25),cv2.FONT_HERSHEY_COMPLEX_SMALL,2,255,2)
-        image, results = mediapipe_detection(cropframe, hands)
-        # print(results)
         
-        # Draw landmarks
-        # draw_styled_landmarks(image, results)
-        # 2. Prediction logic
+        cropframe=frame[40:400,0:300]
+      
+        frame=cv2.rectangle(frame,(0,40),(300,400),255,2)
+       
+        image, results = mediapipe_detection(cropframe, hands)
+        
+        
+        
         keypoints = extract_keypoints(results)
         sequence.append(keypoints)
         sequence = sequence[-30:]
+
+
+
 
         try: 
             if len(sequence) == 30:
@@ -62,8 +71,9 @@ with mp_hands.Hands(
                 print(actions[np.argmax(res)])
                 predictions.append(np.argmax(res))
                 
+
+
                 
-            #3. Viz logic
                 if np.unique(predictions[-10:])[0]==np.argmax(res): 
                     if res[np.argmax(res)] > threshold: 
                         if len(sentence) > 0: 
@@ -78,21 +88,23 @@ with mp_hands.Hands(
                     sentence = sentence[-1:]
                     accuracy=accuracy[-1:]
 
-                # Viz probabilities
-                # frame = prob_viz(res, actions, frame, colors,threshold)
+                
+
         except Exception as e:
-            # print(e)
+            
             pass
             
         cv2.rectangle(frame, (0,0), (300, 40), (245, 117, 16), -1)
         cv2.putText(frame,"Output: -"+' '.join(sentence)+''.join(accuracy), (3,30), 
                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
         
-        # Show to screen
+        
         cv2.imshow('OpenCV Feed', frame)
 
-        # Break gracefully
+       
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
+
+        
     cap.release()
     cv2.destroyAllWindows()
--- trainmodel.py
+++ trainmodel.py
@@ -5,7 +5,9 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense
 from keras.callbacks import TensorBoard
 label_map = {label:num for num, label in enumerate(actions)}
-# print(label_map)
+
+
+
 sequences, labels = [], []
 for action in actions:
     for sequence in range(no_sequences):
@@ -16,9 +18,13 @@ for action in actions:
         sequences.append(window)
         labels.append(label_map[action])
 
+
+
 X = np.array(sequences)
 y = to_categorical(labels).astype(int)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+
+
 
 log_dir = os.path.join('Logs')
 tb_callback = TensorBoard(log_dir=log_dir)
@@ -31,11 +37,15 @@ model.add(Dense(32, activation='relu'))
 model.add(Dense(actions.shape[0], activation='softmax'))
 res = [.7, 0.2, 0.1]
 
+
+
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 model.fit(X_train, y_train, epochs=200, callbacks=[tb_callback])
 model.summary()
 
+
+
 model_json = model.to_json()
 with open("model.json", "w") as json_file:
     json_file.write(model_json)
-model.save('model.h5')
+model.save('smaple.h5')
